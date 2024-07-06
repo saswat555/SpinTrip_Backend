78dc78b948bc6810a1a144d521c0cf288fc55cb1
pratyaym0212 <pratyay.mazumdar02@gmail.com>
2024-07-06 15:36:34 +0530

    host routes get brand

--- Routes/hostRoutes.js
+++ Routes/hostRoutes.js
@@ -174,6 +174,50 @@ router.post('/verify-otp', async (req, res) => {
   }
 });
 
+router.get('/get-brand', async (req, res) => {
+  try {
+    const brands = [];
+    const csvFilePath = path.join(__dirname, '..', 'data', 'brands.csv');
+
+    // Check if the file exists
+    if (!fs.existsSync(csvFilePath)) {
+      console.log(`File not found: ${csvFilePath}`);
+      return res.status(404).json({ message: 'No brands found' });
+    }
+    console.log(`Reading file: ${csvFilePath}`);
+    let brand;
+    fs.createReadStream(csvFilePath)
+      .pipe(csv({ headers: false }))
+      .on('data', (row) => {
+        if(row[1]){
+        brand = {
+          brand_name: row[0], 
+          logo_path: process.env.BASE_URL + row[1]  
+        };
+        }
+        else{
+        brand = {
+            brand_name: row[0], 
+            logo_path: null
+          };
+
+        }
+        console.log(`Read row: ${JSON.stringify(brand)}`);
+        brands.push(brand);
+      })
+      .on('end', () => {
+        res.status(200).json({ brands });
+      })
+      .on('error', (error) => {
+        console.error(`Error reading file: ${error}`);
+        res.status(500).json({ message: 'Error reading CSV file', error });
+      });
+  } catch (error) {
+    console.error(`Error in try-catch: ${error}`);
+    res.status(500).json({ message: 'Error fetching brands', error });
+  }
+});
+
 // Host Signup
 router.post('/signup', async (req, res) => {
   var phone = req.body.phone;
